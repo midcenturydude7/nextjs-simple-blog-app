75fad109fd727446fdfb11ab46b8e207c0e6779f
midCenturyDude7 <mgriffes@gmail.com>
2023-05-18 12:46:26 -0700

    Adding dynamic blog posts

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,20 @@
-const Blog = () => {
-  return <div>Blog Landing Page</div>;
-};
+import { getAllPosts } from "../../lib/cms";
+import Link from "next/link";
 
-export default Blog;
+export default async function Blog() {
+  const posts = await getAllPosts();
+
+  return (
+    <div>
+      {posts.map((post) => {
+        return (
+          <Link href={`/blog/${post.slug}`} key={post.slug}>
+            <div>
+              <h1>{post.title}</h1>
+            </div>
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
